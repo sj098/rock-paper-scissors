e8701f14b14d76dfaae41d3f5bb948f906c46064
Suvin Jeon <suvinjeon@gmail.com>
2022-08-25 18:04:49 -0400

    Edit return comments to add opponent's choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,49 +6,54 @@ function getComputerChoice() {
 // 1 = paper
 // 2 = scissors
 
-//let playerSelection = prompt("Enter your choice from rock, paper, or scissors.");
-
 function playRound(playerSelection, computerSelection) {
     let playerChoice = playerSelection.toLowerCase()
-    if (playerChoice = "rock") {
+    if (playerChoice == "rock") {
         if (computerSelection == 0) {
-            return "It's a tie!"
+            return "The opponent chose rock. \nIt's a tie!"
         }
         else if (computerSelection == 1) {
-            return "You lose! Paper beats Rock!"
+            return "The opponent chose paper. \nYou lose! Paper beats Rock!"
 
         }
         else if (computerSelection == 2) {
-            return "You win! Rock beats Scissors!"
+            return "The opponent chose scissors. \nYou win! Rock beats Scissors!"
         }
     }
 
-    else if (playerChoice = "paper") {
-        if (compChoice == 1) {
-            return "It's a tie!"
+    else if (playerChoice == "paper") {
+        if (computerSelection == 1) {
+            return "The opponent chose paper. \nIt's a tie!"
         }
-        else if (compChoice == 0) {
-            return "You win! Paper beats rock!"
+        else if (computerSelection == 0) {
+            return "The opponent chose rock. \nYou win! Paper beats rock!"
         }   
-        else if (compChoice == 2) {
-            return "You lose! Scissors beats paper!"
+        else if (computerSelection == 2) {
+            return "The opponent chose scissors. \nYou lose! Scissors beats paper!"
         }
     }
 
-    else if (playerChoice = "scissors") {
-        if (compChoice == 2) {
-            return "It's a tie!"
+    else if (playerChoice == "scissors") {
+        if (computerSelection == 2) {
+            return "The opponent chose scissors. \nIt's a tie!"
         }
-        else if (compChoice == 1) {
-            return "You win! Scissors beats paper!"
+        else if (computerSelection == 1) {
+            return "The opponent chose paper. \nYou win! Scissors beats paper!"
         }   
-        else if (compChoice == 0) {
-            return "You lose! Rock beats scissors!"
+        else if (computerSelection == 0) {
+            return "The opponent chose rock. \nYou lose! Rock beats scissors!"
         }
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        let playerSelection = prompt("Enter your choice from rock, paper, or scissors.");
+        let computerSelection = getComputerChoice();
+        console.log("Round " + i);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+console.log(game());
